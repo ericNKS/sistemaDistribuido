f23a4d3a24202e77faabb90bac24e2284b79c378
Eric Santos <ek.silva.santos@gmail.com>
2022-11-29 11:52:49 -0300

    Alterando detalhes

--- gerenteUDP.py
+++ gerenteUDP.py
@@ -2,11 +2,11 @@
 # Cliente Socket UDP
 # ------------------
 
+import json
+import socket
 print("Eu sou um CLIENTE UDP!")
 
 # Importando a biblioteca
-import socket
-import json
 
 # Definindo ip e porta
 HOST = '192.168.0.101'  # Endereco IP do Servidor
@@ -21,62 +21,75 @@ enderecoServidor = (HOST, PORT)
 print("Vou começar a mandar mensagens para o servidor.")
 
 while (True):
-	# Aqui começa a conversa
-	print('''
+    # Aqui começa a conversa
+    print('''
        	*************************************************
 		Codigos de Operação
 		1. Total de vendas de um vendedor
 		2. Total de vendas de uma loja
-		3. Total de vendas em um período 
+		3. Total de vendas em um período
 		4. Melhor vendedor
 		5. Melhor loja
 		6. Encerrar cliente
   		*************************************************
 		''')
-	codOpe = input("Digito o codigo de operação: ")# Colocando as resposta em um dicionarios
-	mensagem = {
-		'codOpe': codOpe + "g",
-	}
-	if codOpe == '1':
-		nomeVendedor = input('Digite o nome do vendedor: ')
-		mensagem['nomeVendedor'] = nomeVendedor.lower()
-	if codOpe == '2':
-		IDLoja = input('Digite a identificação da loja: ')
-		mensagem['IDLoja'] = IDLoja
-	if codOpe == '3':
-		while(True):
-			dataInicial = input('Digite a data inicial de procura (dd/mm/aaaa): ')
-			dataFinal = input('Digite a data final de procura (dd/mm/aaaa): ')
-			if len(dataInicial) == 10 and len(dataFinal) == 10:
-				# Data inicial
-				diaInicial = dataInicial[0] + dataInicial[1]
-				mesInicial = dataInicial[3] + dataInicial[4]
-				anoInicial = dataInicial[6] + dataInicial[7] + dataInicial[8] + dataInicial[9]
-				mensagem['dataInicial'] = f"{anoInicial}-{mesInicial}-{diaInicial}"
-				# Data final
-				diaFinal = dataFinal[0] + dataFinal[1]
-				mesFinal = dataFinal[3] + dataFinal[4]
-				anoFinal = dataFinal[6] + dataFinal[7] + dataFinal[8] + dataFinal[9]
-				mensagem['dataFinal'] = f"{anoFinal}-{mesFinal}-{diaFinal}"
-				break
-			else:
-				print("Formato de uma das datas está errada")
-	elif codOpe == '6':
-		break
+    codOpe = input(
+        "Digito o codigo de operação: ")  # Colocando as resposta em um dicionarios
+    mensagem = {
+        'codOpe': codOpe + "g",
+    }
+    if codOpe == '1':
+        print('''Vendedores:
+		1 - Lucas
+		2 - Joana
+		3- Mateus
+		4- Paula''')
+        codVend = input('Digite o número do vendedor: ')
+        mensagem['nomeVendedor'] = codVend
+    if codOpe == '2':
+        print('''Lojas:
+           1 - Pituba
+           2 - Barra
+           3- Comércio
+           4- Calçada''')
+        codLoja = input('Digite a identificação da loja: ')
+        mensagem['IDLoja'] = codLoja
+    if codOpe == '3':
+        while (True):
+            dataInicial = input(
+                'Digite a data inicial de procura (dd/mm/aaaa): ')
+            dataFinal = input('Digite a data final de procura (dd/mm/aaaa): ')
+            if len(dataInicial) == 10 and len(dataFinal) == 10:
+                # Data inicial
+                diaInicial = dataInicial[0] + dataInicial[1]
+                mesInicial = dataInicial[3] + dataInicial[4]
+                anoInicial = dataInicial[6] + dataInicial[7] + \
+                    dataInicial[8] + dataInicial[9]
+                mensagem['dataInicial'] = f"{anoInicial}-{mesInicial}-{diaInicial}"
+                # Data final
+                diaFinal = dataFinal[0] + dataFinal[1]
+                mesFinal = dataFinal[3] + dataFinal[4]
+                anoFinal = dataFinal[6] + dataFinal[7] + \
+                    dataFinal[8] + dataFinal[9]
+                mensagem['dataFinal'] = f"{anoFinal}-{mesFinal}-{diaFinal}"
+                break
+            else:
+                print("Formato de uma das datas está errada")
+    elif codOpe == '6':
+        break
 
-	# Enviando mensagem ao servidor
- 
-	
-	msgJson = json.dumps(mensagem)
-	print(msgJson)
-	#print("... Vou mandar uma mensagem para o servidor")
-	cliente.connect(enderecoServidor)
-	cliente.sendall(bytes(msgJson,encoding="utf-8"))
+    # Enviando mensagem ao servidor
 
-	# Recebendo resposta do servidor
-	msg, endereco = cliente.recvfrom(9000)
-	resposta = msg.decode("utf-8")
-	print("... O servidor me respondeu:", resposta)
+    msgJson = json.dumps(mensagem)
+    print(msgJson)
+    #print("... Vou mandar uma mensagem para o servidor")
+    cliente.connect(enderecoServidor)
+    cliente.sendall(bytes(msgJson, encoding="utf-8"))
+
+    # Recebendo resposta do servidor
+    msg, endereco = cliente.recvfrom(9000)
+    resposta = msg.decode("utf-8")
+    print("... O servidor me respondeu:", resposta)
 
 print("... Encerrando o cliente")
 cliente.close()
--- vendedorUDP.py
+++ vendedorUDP.py
@@ -5,7 +5,6 @@
 # Importações
 import socket
 import json
-from datetime import date
 
 # Definindo ip e porta
 HOST = '192.168.0.101'  # Endereco IP do Servidor
@@ -19,48 +18,74 @@ enderecoServidor = (HOST, PORT)
 
 
 while (True):
-	# Aqui começa a conversa
-	print('''
+    # Aqui começa a conversa
+    print('''
        	*******************
 		Codigos de Operação
 		1. Realizar Venda
 		5. Encerrar Cliente
   		*******************
 		''')
-	codOpe = input("Digito o codigo de operação: ")
-	if codOpe == '5':
-		print("Operação cancelada!!")
-		break
-	nomeVendedor = input("Digite seu nome, vendedor: ")
-	IDLoja = input("Digite a identificação da loja: ")
-	while(True):
-		dataVenda = input("Data da venda (dd/mm/aaaa): ")
-		if len(dataVenda) == 10:
+    codOpe = input("Digito o codigo de operação: ")
+    if codOpe == '5':
+        print("Operação cancelada!!")
+        break
+    while (True):
+        
+        print('''Vendedores:
+		1 - Lucas 
+		2 - Joana
+		3- Mateus
+		4- Paula''')
+	 	codVend = input("Produto vendido por: (Digite o número)")
+   
+	 	if codVend > 0 or codVend < 5:
 			break
 		else:
-			print("O formato da data está errada")
-	valueVenda = input("Valor da venda: ")
-	# Enviando mensagem ao servidor
-	# Colocando as resposta em um dicionarios
- 
-	mensagem = {
-		'codOpe': codOpe,
-		'nomeVendedor': nomeVendedor.lower(),
-		'IDLoja': IDLoja,
-		'dataVenda': dataVenda,
-		'valorVenda': valueVenda,
-	}
-	print(f"{mensagem['diaVenda']}/{mensagem['mesVenda']}/{mensagem['anoVenda']}")
-	msgJson = json.dumps(mensagem)
-	print(msgJson)
-	#print("... Vou mandar uma mensagem para o servidor")
-	cliente.connect(enderecoServidor)
-	cliente.sendall(bytes(msgJson,encoding="utf-8"))
+            print("Vendedor inválido")
+            
+    while (True):
+        print('''Lojas:
+           1 - Pituba
+           2 - Barra
+           3- Comércio 
+           4- Calçada''')
+        codLoja = input('Produto vendido pela loja: (Digite o número da identificação)')
+        
+        if codLoja > 0 or codLoja < 5:
+            break
+		else:
+            print("Loja inválida")
+            
+    while (True):
+        dataVenda = input("Data da venda (dd/mm/aaaa): ")
+        if len(dataVenda) == 10:
+            break
+        else:
+            print("O formato da data está errada")
+    valueVenda = input("Valor da venda: ")
+    # Enviando mensagem ao servidor
+    # Colocando as resposta em um dicionarios
 
-	# Recebendo resposta do servidor
-	msg, endereco = cliente.recvfrom(9000)
-	resposta = msg.decode("utf-8")
-	print("... O servidor me respondeu:", resposta)
+    mensagem = {
+        'codOpe': codOpe,
+        'nomeVendedor': codVend,
+        'IDLoja': codLoja,
+        'dataVenda': dataVenda,
+        'valorVenda': valueVenda,
+    }
+    print(
+        f"{mensagem['diaVenda']}/{mensagem['mesVenda']}/{mensagem['anoVenda']}")
+    msgJson = json.dumps(mensagem)
+    print(msgJson)
+    #print("... Vou mandar uma mensagem para o servidor")
+    cliente.connect(enderecoServidor)
+    cliente.sendall(bytes(msgJson, encoding="utf-8"))
+
+    # Recebendo resposta do servidor
+    msg, endereco = cliente.recvfrom(9000)
+    resposta = msg.decode("utf-8")
+    print("... O servidor me respondeu:", resposta)
 
 print("... Encerrando o cliente")
 cliente.close()
